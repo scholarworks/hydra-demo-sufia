65ea9aa170209b6cf942f9aba661d7fef09b41c3
Aaron Collier <acollier@calstate.edu>
2016-10-28 16:21:12 -0700

    styling browsing masthead.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,4 @@ Rails.application.config.assets.version = '1.0'
 
 Rails.application.config.assets.precompile += %w( grayscale.css )
 Rails.application.config.assets.precompile += %w( ulms.css )
+Rails.application.config.assets.precompile += %w( csu_sufia.css )
